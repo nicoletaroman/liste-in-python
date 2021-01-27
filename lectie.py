f81f7738a3fb911c1705a36198dfa54f17327707
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2021-01-27 12:02:01 +0200

    Add files via upload

---
+++ lectie.py
@@ -0,0 +1,22 @@
+orase=["Balti","Chisinau","Seoul","Madrid","Bali","Praga"]
+print(orase)
+#del orase[2:3]
+#print(orase)
+#orase.clear()
+#print(orase)
+#orase.remove("Bali")
+#print(orase)
+#orase.pop(3)
+#orase[1:3]=[]
+#orase[ : ]=[]
+#orase.append("Iasi")
+#orase.extend(["Orhei","Kiev"])
+#orase.extend("Balti")
+#orase.insert(1,"Maldive")
+#orase[0]="Bucuresti"
+#orase[:]=["Ciuciulea","Sadovoe","Elizaveta"]
+#x=orase[0:3]
+#n=orase[3:6]
+#print(x)
+#print(n)
+print(orase[-2:]) #ultimile 2 elemente
